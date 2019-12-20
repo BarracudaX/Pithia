caacb7d5aa8521c64bb0998fb7f602d4a53fe023
stylianosst <stylianosplus@gmail.com>
2019-12-20 16:55:25 +0200

    added some fixes to DhlwsiAlgorithmouUI

--- Pithia/src/main/java/com/omada/pithia/ui/controller/DhlwshAlgorithmouController.java
+++ Pithia/src/main/java/com/omada/pithia/ui/controller/DhlwshAlgorithmouController.java
@@ -10,5 +10,19 @@ public class DhlwshAlgorithmouController {
         this.viewController = viewController;
         this.thewria = thewria;
     }
-    public void saveAlgorythmo(double x,double y){thewria.saveAlgorythmo( x,y);}
+    public void saveAlgorythmo(double x,double y){
+        if (x + y <= 1.0) {
+            thewria.saveAlgorythmo(x, y);
+            viewController.requestForShowMessage(" Το ποσοστο της θεωριας ειναι " + x + " Το ποσοστο του εργαστηριου ειναι " + y);
+        }
+        else{
+            viewController.requestForShowErrorMessage("Το συνολο των ποσοστων ειναι μεγαλυτερο του 1.0");
+        }
+
+    }
+    
+    public void requestForBackPage(){
+        viewController.requestForHomeView();
+
+    }
 }
